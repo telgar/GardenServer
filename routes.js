001661afde51683a1b8af8c3f6fd72396fdd2b51
telgar <tyran.elgar@gmail.com>
2017-08-21 22:32:19 +0100

    Updated routes

--- routes.js
+++ routes.js
@@ -28,8 +28,25 @@ exports.setRoutes = function(server) {
     })
 
     // GET /soil
-    server.get('/soil', (req, res) => {
-        let soilDb = low('../soil.json', {
+    server.get('/soil1', (req, res) => {
+        let soilDb = low('../soil1.json', {
+            storage: require('lowdb/lib/storages/file-async')
+        })        
+        const response = soilDb.get('soil')
+            .sortBy(['timestamp'])
+            .reverse()
+            .take(1)
+            .map((o) => { return { 
+                label: moment(o.timestamp).format('MMMM Do, h:mm:ss a'), 
+                moisture: math.roundToClosest(o.moisture, 100) } })
+            .value()
+
+        res.send(response)
+    })
+
+    // GET /soil
+    server.get('/soil2', (req, res) => {
+        let soilDb = low('../soil2.json', {
             storage: require('lowdb/lib/storages/file-async')
         })        
         const response = soilDb.get('soil')
@@ -65,8 +82,28 @@ exports.setRoutes = function(server) {
     })
 
     // GET /soil/hour
-    server.get('/soil/hour', (req, res) => {
-        let soilDb = low('../soil.json', {
+    server.get('/soil1/hour', (req, res) => {
+        let soilDb = low('../soil1.json', {
+            storage: require('lowdb/lib/storages/file-async')
+        })        
+        const response = soilDb.get('soil')
+            .sortBy(['timestamp'])
+            .reverse()
+            .take(constants.HOUR / constants.SAMPLE_RATE)
+            .groupBy((o) => moment(math.roundToClosestMinute(o.timestamp, 5)).format('h:mm a'))
+            .toPairs()
+            .map((o) => { return _.zipObject(["label", "values"], o); } )
+            .map((o) => { return { 
+                label: o.label, 
+                moisture: math.roundToClosest(_.mean(o.values.map(x => x.moisture)), 100) } })
+            .value()
+
+        res.send(response)
+    })
+
+    // GET /soil/hour
+    server.get('/soil2/hour', (req, res) => {
+        let soilDb = low('../soil2.json', {
             storage: require('lowdb/lib/storages/file-async')
         })        
         const response = soilDb.get('soil')
@@ -105,8 +142,28 @@ exports.setRoutes = function(server) {
     })
 
     // GET /soil/day
-    server.get('/soil/day', (req, res) => {
-        let soilDb = low('../soil.json', {
+    server.get('/soil1/day', (req, res) => {
+        let soilDb = low('../soil1.json', {
+            storage: require('lowdb/lib/storages/file-async')
+        })        
+        const response = soilDb.get('soil')
+            .sortBy(['timestamp'])
+            .reverse()
+            .take(constants.DAY / constants.SAMPLE_RATE)
+            .groupBy((o) => moment(math.roundToClosestMinute(o.timestamp, 30)).format('MMMM Do, h:mm a'))
+            .toPairs()
+            .map((o) => { return _.zipObject(["label", "values"], o); } )
+            .map((o) => { return { 
+                label: o.label, 
+                moisture: math.roundToClosest(_.mean(o.values.map(x => x.moisture)), 100) } })
+            .value()
+
+        res.send(response)
+    })
+
+    // GET /soil/day
+    server.get('/soil2/day', (req, res) => {
+        let soilDb = low('../soil2.json', {
             storage: require('lowdb/lib/storages/file-async')
         })        
         const response = soilDb.get('soil')
@@ -145,8 +202,28 @@ exports.setRoutes = function(server) {
     })
 
     // GET /soil/week
-    server.get('/soil/week', (req, res) => {
-        let soilDb = low('../soil.json', {
+    server.get('/soil1/week', (req, res) => {
+        let soilDb = low('../soil1.json', {
+            storage: require('lowdb/lib/storages/file-async')
+        })        
+        const response = soilDb.get('soil')
+            .sortBy(['timestamp'])
+            .reverse()
+            .take(constants.WEEK / constants.SAMPLE_RATE)
+            .groupBy((o) => moment(math.roundToClosestMinute(o.timestamp, 60)).format('MMMM Do, h:mm a'))
+            .toPairs()
+            .map((o) => { return _.zipObject(["label", "values"], o); } )
+            .map((o) => { return { 
+                label: o.label, 
+                moisture: math.roundToClosest(_.mean(o.values.map(x => x.moisture)), 100) } })
+            .value()
+
+        res.send(response)
+    })
+
+    // GET /soil/week
+    server.get('/soil2/week', (req, res) => {
+        let soilDb = low('../soil2.json', {
             storage: require('lowdb/lib/storages/file-async')
         })        
         const response = soilDb.get('soil')
@@ -184,8 +261,27 @@ exports.setRoutes = function(server) {
     })
 
     // GET /soil/all-time
-    server.get('/soil/all-time', (req, res) => {
-        let soilDb = low('../soil.json', {
+    server.get('/soil1/all-time', (req, res) => {
+        let soilDb = low('../soil1.json', {
+            storage: require('lowdb/lib/storages/file-async')
+        })        
+        const response = soilDb.get('soil')
+            .sortBy(['timestamp'])
+            .reverse()
+            .groupBy((o) => moment(math.roundToClosestMinute(o.timestamp, 60)).format('MMMM Do, h:mm a'))
+            .toPairs()
+            .map((o) => { return _.zipObject(["label", "values"], o); } )
+            .map((o) => { return { 
+                label: o.label, 
+                moisture: math.roundToClosest(_.mean(o.values.map(x => x.moisture)), 100) } })
+            .value()    
+
+        res.send(response)
+    })
+
+    // GET /soil/all-time
+    server.get('/soil2/all-time', (req, res) => {
+        let soilDb = low('../soil2.json', {
             storage: require('lowdb/lib/storages/file-async')
         })        
         const response = soilDb.get('soil')
